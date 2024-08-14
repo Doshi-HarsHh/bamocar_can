e4cba88e395164b7646b0d8480b6971c6c12d9db
Harsh <115067899+Doshi-HarsHh@users.noreply.github.com>
2024-08-14 10:33:09 +0530

    Complete all func

---
+++ bamocar_can.c
@@ -0,0 +1,8 @@
+#include "bamocar_can.h"
+
+void readSpeed(bool data_in_loop);
+void readAllTemp(bool data_in_loop);
+void readCurrent(bool data_in_loop);
+void readVdc_bus(bool data_in_loop);
+void readTorqueref(bool data_in_loop);
+//complete all the required func
